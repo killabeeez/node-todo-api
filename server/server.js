d347cc206db54c34d03b193cd4d6dd2735300511
alexandar43 <alexandar43@hotmail.com>
2018-12-21 20:06:36 +0100

    Tests for post todos route

--- server/server.js
+++ server/server.js
@@ -29,34 +29,4 @@ app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
 
-// var newTodo = new Todo({
-//   text: 'cook dinner'
-// });
-
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo', doc)
-// }, e => {
-//   console.log('Unable to save todo', e)
-// });
-
-// var someTodo = new Todo({
-//   text: 'feed the cat'
-// });
-
-// someTodo.save().then((doc) => {
-//   console.log(JSON.stringify(doc, undefined, 2))
-// }, e => {
-//   console.log('Unable to save todo', e)
-// });
-
-
-
-// var newUser = new User({
-//   email: 'radec43@hotmail.com'
-// });
-
-// newUser.save().then((doc) => {
-//   console.log('Created new user', doc);
-// }, e => {
-//   console.log('Unable to add new user', e);
-// })
+module.exports = { app };
